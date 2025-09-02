c5404a4a12c04d1e700bd6ec7dd61fba2f881e35
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-09-02 17:42:07 +0500

    Update Task10.java

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -3,13 +3,16 @@ package com.example.task10;
 public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
-
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
         
-        return a == b;
-
+        if (Float.isNaN(a) && Float.isNaN(b)) return true;
+        if (Float.isInfinite(a) && Float.isInfinite(b)) return a == b; // +Inf == +Inf, -Inf == -Inf
+        
+        double scale = Math.pow(10, precision);
+        
+        long scaledA = Math.round(a * scale);
+        long scaledB = Math.round(b * scale);
+        
+        return scaledA == scaledB;
     }
 
     public static void main(String[] args) {
